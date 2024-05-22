ae3f4f3b20bf185f621af64e9bc4653218a53646
Neo <neo@neopolitan.me>
2024-05-22 15:20:50 -0300

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import ScrollTop from "./components/ScrollToTop";
+import Script from 'next/script';
+
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["300", "500", "600"], style: ['normal'] });
 
@@ -27,6 +29,19 @@ export default function RootLayout({
         {children}
         <ScrollTop />
       </body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16569630788"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-16569630788');
+        `}
+      </Script>
     </html>
   );
 }
